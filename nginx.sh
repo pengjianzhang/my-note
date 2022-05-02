a50694c7321a8a4f0ec482e629ccdeae9f299555
Jianzhang Peng <pengjianzhang@gmail.com>
2022-05-02 22:35:03 +0800

    nginx build

--- nginx.sh
+++ nginx.sh
@@ -2,14 +2,14 @@
 
 yum install  pcre2-devel pcre-devel
 
-#rm -rf nginx
-#mkdir -p nginx
+rm -rf nginx
+mkdir -p nginx
 cd nginx
 
 function download()
 {
-#    wget https://nginx.org/download/nginx-1.20.2.tar.gz
-#    wget https://github.com/openresty/echo-nginx-module/archive/refs/heads/master.zip
+    wget https://nginx.org/download/nginx-1.20.2.tar.gz
+    wget https://github.com/openresty/echo-nginx-module/archive/refs/heads/master.zip
 
     tar -zxvf nginx-1.20.2.tar.gz
     unzip master.zip
